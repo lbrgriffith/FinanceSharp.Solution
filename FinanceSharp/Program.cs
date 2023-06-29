e527e76d55d39b0561399b2efd8f66369b9c7fe2
Ricardo Griffith <ricardo.griffith@icloud.com>
2023-06-29 07:45:39 -0300

    Implemented simple interest

--- FinanceSharp/Program.cs
+++ FinanceSharp/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace FinanceSharp
+﻿namespace FinanceSharp;
+
+using CommandLine;
+using Options;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            
-        }
+        Parser.Default.ParseArguments<SimpleInterestOption>(args)
+            .MapResult(
+              (SimpleInterestOption opts) =>
+              {
+                      decimal interest = CalculateSimpleInterest(opts.Principal, 
+                                                                 opts.Rate, 
+                                                                 opts.Time);
+                      Console.WriteLine($"Simple interest: {interest}");
+                  return interest;
+              },
+              errs => 1);
+    }
+
+    static decimal CalculateSimpleInterest(decimal principal, decimal rate, decimal time)
+    {
+        // formula to calculate simple interest: PRT/100
+        return (principal * rate * time) / 100;
     }
 }
