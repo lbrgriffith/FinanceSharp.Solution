0d41a07b1004e5c6ec76ae8fc29dc6131eeefc97
Ricardo Griffith <ricardo.griffith@icloud.com>
2023-06-29 18:01:35 -0300

    Created PresentValue Option class.

---
+++ FinanceSharp/Options/PresentValue.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+
+namespace FinanceSharp.Options;
+
+[Verb("present-value", HelpText = "Calculates present value.")]
+internal class PresentValue
+{
+    [Option('f', "future-value", Required = false, HelpText = "The future value.")]
+    public decimal? FutureValue { get; set; }
+
+    [Option('r', "rate", Required = false, HelpText = "The interest rate.")]
+    public decimal? Rate { get; set; }
+
+    [Option('t', "time", Required = false, HelpText = "The time in years.")]
+    public int? Time { get; set; }
+}
