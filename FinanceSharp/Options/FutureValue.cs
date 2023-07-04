2fa0dd6d32f69f32d733c8cae97e15f60b98dc86
Ricardo Griffith <ricardo.griffith@icloud.com>
2023-07-04 20:10:46 -0300

    Added FutureValue option class.

---
+++ FinanceSharp/Options/FutureValue.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+namespace FinanceSharp.Options;
+
+[Verb("future-value", HelpText = "Calculates future value.")]
+internal class FutureValue
+{
+    [Option('p', "principal", Required = false, HelpText = "The principal amount.")]
+    public decimal? Principal { get; set; }
+
+    [Option('r', "rate", Required = false, HelpText = "The interest rate.")]
+    public decimal? Rate { get; set; }
+
+    [Option('t', "time", Required = false, HelpText = "The time in years.")]
+    public decimal? Time { get; set; }
+
+    [Option('f', "frequency", Required = false, HelpText = "The compounding frequency.")]
+    public decimal Frequency { get; set; }
+}
